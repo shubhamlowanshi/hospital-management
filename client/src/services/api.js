489efdba76298182aa14669661c1bc804cc5e519
shubhamlowanshi2 <113835290+shubhamlowanshi@users.noreply.github.com>
2025-12-16 00:49:52 +0530

    fix: remove hardcoded PORT

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // Create axios instance with base configuration
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: "https://my-auth-api.onrender.com/api/test"
+,
   headers: {
     'Content-Type': 'application/json',
   },

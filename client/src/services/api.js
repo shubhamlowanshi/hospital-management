bafaa272455461a275f4fcc6796ae6bcc18a919a
shubhamlowanshi2 <113835290+shubhamlowanshi@users.noreply.github.com>
2025-12-16 12:20:34 +0530

    fix: add /api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base configuration
 const api = axios.create({
-  baseURL: "https://hospital-management-09e8.onrender.com"
+  baseURL: "https://hospital-management-09e8.onrender.com/api"
 ,
   headers: {
     'Content-Type': 'application/json',
